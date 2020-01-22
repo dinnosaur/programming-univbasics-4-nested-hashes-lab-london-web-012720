d3738488dd228701412dd69d766d03857b40fcc6
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-22 22:43:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,6 @@ def adding_to_dennis
           :languages => ["C",""]
         }
      }
- programmer_hash[:dennis_ritchie][:language][1]="Assembly"
+ programmer_hash[:dennis_ritchie][:language][1]= "Assembly"
  return programmer_hash
 end
